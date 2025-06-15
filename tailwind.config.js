2fff3efff85b26151bf9026121a04a6ce428dc24
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2025-06-15 15:48:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4CAF50",
+        secondary: "#FFFFFF",
+        textMain: "#333333",
+        textSecondary: "#888888",
+        hover: "#45A049",
+        border: "#E0E0E0",
+      },
+    },
+  },
+  plugins: [],
+};
